aa703818b856bd4cf53982a93df9c18341a2bd34
Zeprus <zeprusaliastrylon@googlemail.com>
2022-06-20 16:20:59 +0200

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MTDAssetLoading
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "MTDAssetLoading";
-        public const string PLUGIN_NAME = "MTDAssetLoading";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
